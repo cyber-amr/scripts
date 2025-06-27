46e308501e94ae224165f9ad2a46611e371448da
Amr <amr@programmer.net>
2025-06-27 17:38:29 +0300

    `shutdown` option

--- quit.sh
+++ quit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-case "$(printf "kill\nreboot\npoweroff" | dmenu -b -i)" in
+case "$(printf "shutdown\nkill\nreboot\npoweroff" | dmenu -b -i)" in
+	shutdown) doas shutdown now ;;
 	kill) ps -u $USER -o pid,comm,%cpu,%mem | dmenu -b -i -l 15 -p 'Kill:' | awk '{print $1}' | xargs -r kill ;;
 	reboot) doas reboot -i ;;
 	poweroff) doas poweroff ;;
