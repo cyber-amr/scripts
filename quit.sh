21a8a3da12cedf36fa5594c3dce90965ecdab556
Amr <amr@programmer.net>
2025-06-27 17:40:47 +0300

    rm reboot `-i` flag, not supported on void

--- quit.sh
+++ quit.sh
@@ -3,7 +3,7 @@
 case "$(printf "shutdown\nkill\nreboot\npoweroff" | dmenu -b -i)" in
 	shutdown) doas shutdown now ;;
 	kill) ps -u $USER -o pid,comm,%cpu,%mem | dmenu -b -i -l 15 -p 'Kill:' | awk '{print $1}' | xargs -r kill ;;
-	reboot) doas reboot -i ;;
+	reboot) doas reboot ;;
 	poweroff) doas poweroff ;;
 	*) exit 1 ;;
 esac
